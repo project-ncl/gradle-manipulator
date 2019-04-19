38a808d87e6471f08fddf33334638c30ccfc6fa3
Chris Laprun <metacosm@gmail.com>
2019-04-19 22:37:05 +0200

    feat: make manipulation plugin automatically apply maven-publish plugin

--- manipulation/src/main/java/org/jboss/gm/manipulation/ManipulationPlugin.java
+++ manipulation/src/main/java/org/jboss/gm/manipulation/ManipulationPlugin.java
@@ -4,6 +4,7 @@ import static org.jboss.gm.common.alignment.AlignmentUtils.getCurrentAlignmentMo
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.publish.maven.plugins.MavenPublishPlugin;
 import org.jboss.gm.common.alignment.AlignmentModel;
 import org.jboss.gm.manipulation.actions.ManifestUpdateAction;
 import org.jboss.gm.manipulation.actions.OverrideDependenciesAction;
@@ -16,11 +17,12 @@ public class ManipulationPlugin implements Plugin<Project> {
 
     @Override
     public void apply(Project project) {
-        final String projectName = project.getName();
+        // apply the maven publishing plugin
+        project.getPluginManager().apply(MavenPublishPlugin.class);
 
         // get the previously performed alignment
         final AlignmentModel alignmentModel = getCurrentAlignmentModel(project);
-        final AlignmentModel.Module correspondingModule = alignmentModel.findCorrespondingModule(projectName);
+        final AlignmentModel.Module correspondingModule = alignmentModel.findCorrespondingModule(project.getName());
 
         // add actions to manipulate project
         project.afterEvaluate(new ProjectChangeVersionAction(correspondingModule));

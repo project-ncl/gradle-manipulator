32e267afff5458a6e9abf48391edd01e8f77936f
Georgios Andrianakis <geoand@gmail.com>
2019-04-23 08:54:56 +0300

    Add idea plugin (#15)
    This makes it easy to see the sources of Gradle itself

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.diffplug.gradle.spotless") version "3.21.0"
+    idea
 }
 
 allprojects {
@@ -15,6 +16,7 @@ subprojects {
     apply(plugin = "maven-publish")
     apply(plugin = "java-gradle-plugin")
     apply(plugin = "com.diffplug.gradle.spotless")
+    apply(plugin = "idea")
 
     spotless {
         java {
@@ -25,5 +27,11 @@ subprojects {
 
     tasks.withType<JavaCompile>().configureEach {
         dependsOn("spotlessApply")
+    }
+
+    idea {
+        module {
+            setDownloadSources(true)
+        }
     }
 }

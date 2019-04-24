3d0fdab79176ff1b3773703d09909b5be125580d
Chris Laprun <metacosm@gmail.com>
2019-04-24 16:12:10 +0200

    refactor: extract align method

--- analyzer/src/functTest/java/org/jboss/gm/analyzer/alignment/TestUtils.java
+++ analyzer/src/functTest/java/org/jboss/gm/analyzer/alignment/TestUtils.java
@@ -1,11 +1,19 @@
 package org.jboss.gm.analyzer.alignment;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
 
 import org.apache.commons.io.FileUtils;
+import org.gradle.testkit.runner.BuildResult;
+import org.gradle.testkit.runner.GradleRunner;
+import org.gradle.testkit.runner.TaskOutcome;
+import org.jboss.gm.common.alignment.AlignmentUtils;
+import org.jboss.gm.common.alignment.ManipulationModel;
+import org.junit.rules.TemporaryFolder;
 
 public final class TestUtils {
 
@@ -15,5 +23,22 @@ public final class TestUtils {
     public static void copyDirectory(String classpathResource, File target) throws URISyntaxException, IOException {
         FileUtils.copyDirectory(Paths
                 .get(TestUtils.class.getClassLoader().getResource(classpathResource).toURI()).toFile(), target);
+    }
+
+    static ManipulationModel align(TemporaryFolder tempDir, String projectDirName) throws IOException, URISyntaxException {
+        final File projectRoot = tempDir.newFolder(projectDirName);
+        copyDirectory(projectDirName, projectRoot);
+        assertThat(projectRoot.toPath().resolve("build.gradle")).exists();
+
+        final BuildResult buildResult = GradleRunner.create()
+                .withProjectDir(projectRoot)
+                .withArguments(AlignmentTask.NAME)
+                .withDebug(true)
+                .withPluginClasspath()
+                .build();
+
+        assertThat(buildResult.task(":" + AlignmentTask.NAME).getOutcome()).isEqualTo(TaskOutcome.SUCCESS);
+
+        return AlignmentUtils.getAlignmentModelAt(projectRoot.toPath().toFile());
     }
 }

28eb2f55b488a63202267d19519dee65a8f4bac8
Georgios Andrianakis <geoand@gmail.com>
2019-05-07 17:43:25 +0300

    Use resolved dependencies when no version is present

---
+++ analyzer/src/functTest/java/org/jboss/gm/analyzer/alignment/SimpleProjectWithNebulaPluginFunctionalTest.java
@@ -0,0 +1,70 @@
+package org.jboss.gm.analyzer.alignment;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Collection;
+
+import org.commonjava.maven.atlas.ident.ref.ProjectVersionRef;
+import org.gradle.api.Project;
+import org.jboss.gm.common.Configuration;
+import org.jboss.gm.common.alignment.ManipulationModel;
+import org.jboss.gm.common.alignment.Utils;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.RestoreSystemProperties;
+import org.junit.rules.TemporaryFolder;
+import org.junit.rules.TestRule;
+
+public class SimpleProjectWithNebulaPluginFunctionalTest extends AbstractWiremockTest {
+
+    @Rule
+    public final TestRule restoreSystemProperties = new RestoreSystemProperties();
+
+    @Rule
+    public TemporaryFolder tempDir = new TemporaryFolder();
+
+    @Before
+    public void setup() {
+        stubFor(post(urlEqualTo("/da/rest/v-1/reports/lookup/gavs"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json;charset=utf-8")
+                        .withBody(readSampleDAResponse("simple-project-with-nebula-plugin-da-response.json"))));
+
+        System.setProperty(Configuration.DA, "http://127.0.0.1:" + AbstractWiremockTest.PORT + "/da/rest/v-1");
+    }
+
+    @Test
+    public void ensureAlignmentFileCreated() throws IOException, URISyntaxException {
+        final File projectRoot = tempDir.newFolder("simple-project-with-nebula-plugin");
+        final ManipulationModel alignmentModel = TestUtils.align(projectRoot, projectRoot.getName());
+
+        assertTrue(new File(projectRoot, AlignmentTask.GME).exists());
+        assertEquals(AlignmentTask.LOAD_GME, Utils.getLastLine(new File(projectRoot, Project.DEFAULT_BUILD_FILE)));
+
+        assertThat(alignmentModel).isNotNull().satisfies(am -> {
+            assertThat(am.getGroup()).isEqualTo("org.acme.gradle");
+            assertThat(am.getName()).isEqualTo("root");
+            assertThat(am.findCorrespondingChild("root")).satisfies(root -> {
+                assertThat(root.getVersion()).isEqualTo("1.0.1.redhat-00001");
+                assertThat(root.getName()).isEqualTo("root");
+                final Collection<ProjectVersionRef> alignedDependencies = root.getAlignedDependencies().values();
+                assertThat(alignedDependencies)
+                        .extracting("artifactId", "versionString")
+                        .containsOnly(
+                                tuple("spring-core", "4.3.22.RELEASE-redhat-1"));
+            });
+        });
+    }
+}
--- analyzer/src/main/java/org/jboss/gm/analyzer/alignment/AlignmentTask.java
+++ analyzer/src/main/java/org/jboss/gm/analyzer/alignment/AlignmentTask.java
@@ -15,10 +15,14 @@ import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+import org.apache.commons.lang.StringUtils;
 import org.commonjava.maven.atlas.ident.ref.ProjectVersionRef;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.result.DependencyResult;
+import org.gradle.api.artifacts.result.ResolutionResult;
 import org.gradle.api.internal.artifacts.dependencies.DefaultSelfResolvingDependency;
+import org.gradle.api.internal.artifacts.result.DefaultResolvedDependencyResult;
 import org.gradle.api.tasks.TaskAction;
 import org.jboss.gm.common.ProjectVersionFactory;
 import org.jboss.gm.common.alignment.ManipulationModel;
@@ -37,7 +41,7 @@ public class AlignmentTask extends DefaultTask {
     static final Set<String> projectsToAlign = new HashSet<>();
 
     private final Logger logger = getLogger();
-    
+
     @TaskAction
     public void perform() {
         final Project project = getProject();
@@ -103,17 +107,55 @@ public class AlignmentTask extends DefaultTask {
 
     private Collection<ProjectVersionRef> getAllProjectDependencies(Project project) {
         final Set<ProjectVersionRef> result = new LinkedHashSet<>();
-        project.getConfigurations().all(configuration -> configuration.getAllDependencies().forEach(dep -> {
-            if (dep instanceof DefaultSelfResolvingDependency) {
-                logger.warn("Ignoring dependency of type {} on project {}", dep.getClass().getName(), project.getName());
-            } else if (isEmpty(dep.getVersion())) {
-                logger.warn("Ignoring empty version on dependency {} on project {}", dep.toString(), project.getName());
-            } else {
-                result.add(ProjectVersionFactory.withGAVAndConfiguration(dep.getGroup(), dep.getName(), dep.getVersion(),
-                        configuration.getName()));
-            }
-        }));
+        project.getConfigurations().all(configuration -> {
+            final ResolutionResult resolutionResult = configuration.getIncoming()
+                    .getResolutionResult();//force dependency resolution - this is needed later on if we encounter deps with no version
+            configuration.getAllDependencies().forEach(dep -> {
+                if (dep instanceof DefaultSelfResolvingDependency) {
+                    logger.warn("Ignoring dependency of type {} on project {}", dep.getClass().getName(), project.getName());
+                } else if (isEmpty(dep.getVersion())) {
+                    // look up resolved dependency with a toString hack
+                    boolean found = false;
+                    final Set<? extends DependencyResult> resolvedDependencies = getAllDependenciesFromResolutionResult(
+                            resolutionResult);
+                    for (DependencyResult resolvedDependency : resolvedDependencies) {
+                        if (!(resolvedDependency instanceof DefaultResolvedDependencyResult)) {
+                            continue;
+                        }
+
+                        final String resolvedDependencyDisplayName = ((DefaultResolvedDependencyResult) resolvedDependency)
+                                .getSelected()
+                                .getId().getDisplayName();
+                        // the display name should be something like: "org.example:somelib:1.2.3"
+                        if (resolvedDependencyDisplayName.startsWith(dep.getGroup() + ":" + dep.getName()) && StringUtils
+                                .countMatches(resolvedDependencyDisplayName, ":") == 2) {
+                            final int i = resolvedDependencyDisplayName.lastIndexOf(":");
+                            if (i != -1) {
+                                result.add(ProjectVersionFactory.withGAVAndConfiguration(dep.getGroup(), dep.getName(),
+                                        resolvedDependencyDisplayName.substring(i + 1),
+                                        configuration.getName()));
+                                found = true;
+                            }
+                        }
+                    }
+                    if (!found) {
+                        logger.warn("Ignoring empty version on dependency {} on project {}", dep.toString(), project.getName());
+                    }
+                } else {
+                    result.add(ProjectVersionFactory.withGAVAndConfiguration(dep.getGroup(), dep.getName(), dep.getVersion(),
+                            configuration.getName()));
+                }
+            });
+        });
         return result;
+    }
+
+    private Set<? extends DependencyResult> getAllDependenciesFromResolutionResult(ResolutionResult resolutionResult) {
+        try {
+            return resolutionResult.getAllDependencies();
+        } catch (Exception e) {
+            return new HashSet<>();
+        }
     }
 
     private void updateModuleDependencies(ManipulationModel correspondingModule,

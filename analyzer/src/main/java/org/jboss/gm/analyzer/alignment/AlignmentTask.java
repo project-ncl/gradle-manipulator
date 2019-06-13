a2b2066afc76b1a0a88fac9af1058b21543dbd30
Nick Cross <ncross@redhat.com>
2019-06-13 17:40:16 +0100

    Add missing comma

--- analyzer/src/main/java/org/jboss/gm/analyzer/alignment/AlignmentTask.java
+++ analyzer/src/main/java/org/jboss/gm/analyzer/alignment/AlignmentTask.java
@@ -63,7 +63,7 @@ public class AlignmentTask extends DefaultTask {
     static final String INJECT_GME_START = "buildscript { apply from: \"gme.gradle\" }";
     static final String GME = "gme.gradle";
     static final String GME_REPOS = "gme-repos.gradle";
-    static final String APPLY_GME_REPOS = "buildscript { apply from: \"${project.rootDir}/gme-repos.gradle\" to: buildscript }";
+    static final String APPLY_GME_REPOS = "buildscript { apply from: \"${project.rootDir}/gme-repos.gradle\", to: buildscript }";
     static final String INJECT_GME_END = "apply from: \"gme-pluginconfigs.gradle\"";
     static final String GME_PLUGINCONFIGS = "gme-pluginconfigs.gradle";
     static final String NAME = "generateAlignmentMetadata";

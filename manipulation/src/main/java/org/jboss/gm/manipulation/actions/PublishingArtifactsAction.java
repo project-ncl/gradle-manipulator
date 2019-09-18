3fe36556871b9464759eb26baf54c89f4902c89b
Tomas Hofman <thofman@redhat.com>
2019-09-18 15:11:46 +0200

    NCL-5099 Fix for publishing when module has no java component

--- manipulation/src/main/java/org/jboss/gm/manipulation/actions/PublishingArtifactsAction.java
+++ manipulation/src/main/java/org/jboss/gm/manipulation/actions/PublishingArtifactsAction.java
@@ -2,9 +2,12 @@ package org.jboss.gm.manipulation.actions;
 
 import org.gradle.api.Action;
 import org.gradle.api.Project;
+import org.gradle.api.component.SoftwareComponent;
+import org.gradle.api.logging.Logger;
 import org.gradle.api.publish.PublicationContainer;
 import org.gradle.api.publish.PublishingExtension;
 import org.gradle.api.publish.maven.MavenPublication;
+import org.jboss.gm.common.logging.GMLogger;
 
 /**
  * <p>
@@ -26,24 +29,31 @@ import org.gradle.api.publish.maven.MavenPublication;
  * </pre>
  */
 public class PublishingArtifactsAction implements Action<Project> {
+
+    private final Logger logger = GMLogger.getLogger(getClass());
+
     @Override
     public void execute(Project project) {
-        project.getExtensions().configure(PublishingExtension.class, new Action<PublishingExtension>() {
-            @Override
-            public void execute(PublishingExtension publishingExtension) {
-                publishingExtension.publications(new Action<PublicationContainer>() {
-                    @Override
-                    public void execute(PublicationContainer publications) {
-                        publications.create("mavenJava", MavenPublication.class, new Action<MavenPublication>() {
-                            @Override
-                            public void execute(MavenPublication mavenPublication) {
-                                mavenPublication.from(project.getComponents().getByName("java"));
-                                // TODO: why are optional dependencies missing in generated pom.xml?
-                            }
-                        });
-                    }
-                });
-            }
-        });
+        SoftwareComponent javaComponent = project.getComponents().findByName("java");
+        if (javaComponent != null) {
+            project.getExtensions().configure(PublishingExtension.class, new Action<PublishingExtension>() {
+                @Override
+                public void execute(PublishingExtension publishingExtension) {
+                    publishingExtension.publications(new Action<PublicationContainer>() {
+                        @Override
+                        public void execute(PublicationContainer publications) {
+                            publications.create("mavenJava", MavenPublication.class, new Action<MavenPublication>() {
+                                @Override
+                                public void execute(MavenPublication mavenPublication) {
+                                    mavenPublication.from(javaComponent);
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        } else {
+            logger.warn("No java component found for project {}, no artifact to publish.", project.getName());
+        }
     }
 }

112724eaf5f91bce35823bdd659f018af7e42492
Nick Cross <ncross@redhat.com>
2020-09-17 15:30:49 +0100

    Add instanceof check for LoggingRule

--- common/src/testFixtures/java/org/jboss/gm/common/rules/LoggingRule.java
+++ common/src/testFixtures/java/org/jboss/gm/common/rules/LoggingRule.java
@@ -6,6 +6,7 @@ import org.gradle.internal.logging.slf4j.OutputEventListenerBackedLoggerContext;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
+import org.slf4j.ILoggerFactory;
 import org.slf4j.LoggerFactory;
 
 /**
@@ -24,20 +25,23 @@ public class LoggingRule implements TestRule {
         return new Statement() {
             @Override
             public void evaluate() throws Throwable {
-                OutputEventListenerBackedLoggerContext context = (OutputEventListenerBackedLoggerContext) LoggerFactory
-                        .getILoggerFactory();
 
-                try {
-                    context.setLevel(level);
-                    if (context.getOutputEventListener() instanceof OutputEventRenderer) {
-                        // Ensure we attach System out/err so any SystemOutRule work.
-                        ((OutputEventRenderer) context.getOutputEventListener()).attachSystemOutAndErr();
-                        ((OutputEventRenderer) context.getOutputEventListener()).configure(level);
+                ILoggerFactory factory = LoggerFactory.getILoggerFactory();
+
+                if (factory instanceof OutputEventListenerBackedLoggerContext) {
+                    OutputEventListenerBackedLoggerContext context = (OutputEventListenerBackedLoggerContext)factory;
+                    try {
+                        context.setLevel(level);
+                        if (context.getOutputEventListener() instanceof OutputEventRenderer) {
+                            // Ensure we attach System out/err so any SystemOutRule work.
+                            ((OutputEventRenderer) context.getOutputEventListener()).attachSystemOutAndErr();
+                            ((OutputEventRenderer) context.getOutputEventListener()).configure(level);
+                        }
+                        base.evaluate();
+                    } finally {
+                        // Could also call setLevel in reverse but reset is probably more comprehensive
+                        context.reset();
                     }
-                    base.evaluate();
-                } finally {
-                    // Could also call setLevel in reverse but reset is probably more comprehensive
-                    context.reset();
                 }
             }
         };

cce2707fe55fc72765cc1a3b1c2a8be03a71ba66
Nick Cross <ncross@redhat.com>
2020-11-25 15:54:38 +0000

    NCL-5886 Update PME to 4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -124,7 +124,7 @@ subprojects {
     extra["logbackVersion"] = "1.2.3"
     extra["mavenVersion"] = "3.5.0"
     extra["ownerVersion"] = "1.0.12"
-    extra["pmeVersion"] = "4.1"
+    extra["pmeVersion"] = "4.2"
     extra["slf4jVersion"] = "1.7.30"
     extra["systemRulesVersion"] = "1.19.0"
 

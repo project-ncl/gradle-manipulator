aa3a57230ca29a88724df4af516d7906d52df588
Nick Cross <ncross@redhat.com>
2021-04-22 21:40:02 +0100

    NCL-6520 Use ContextClassLoaderLocal Translator cache

--- common/src/main/java/org/jboss/gm/common/utils/RESTUtils.java
+++ common/src/main/java/org/jboss/gm/common/utils/RESTUtils.java
@@ -1,6 +1,8 @@
 package org.jboss.gm.common.utils;
 
 import lombok.experimental.UtilityClass;
+
+import org.apache.commons.beanutils.ContextClassLoaderLocal;
 import org.commonjava.maven.ext.io.rest.DefaultTranslator;
 import org.commonjava.maven.ext.io.rest.Translator;
 import org.jboss.gm.common.Configuration;
@@ -8,11 +10,21 @@ import org.jboss.gm.common.Configuration;
 @UtilityClass
 public class RESTUtils {
 
-    private static Translator restCache;
+    private static final ContextClassLoaderLocalWithConfiguration cache = new ContextClassLoaderLocalWithConfiguration();
 
-    public static synchronized Translator getTranslator(Configuration configuration) {
-        if (restCache == null) {
-            restCache = new DefaultTranslator(
+    public static synchronized Translator getTranslator(Configuration config) {
+
+        cache.configuration = config;
+
+        return cache.get();
+    }
+
+    private static class ContextClassLoaderLocalWithConfiguration extends ContextClassLoaderLocal<Translator> {
+        Configuration configuration = null;
+
+        @Override
+        protected Translator initialValue() {
+            return new DefaultTranslator(
                     configuration.daEndpoint(),
                     configuration.restMaxSize(),
                     Translator.CHUNK_SPLIT_COUNT,
@@ -23,6 +35,5 @@ public class RESTUtils {
                     configuration.restSocketTimeout(),
                     configuration.restRetryDuration());
         }
-        return restCache;
     }
 }

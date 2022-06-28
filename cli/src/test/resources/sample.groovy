c256cdcd10af73a02fc5c275f7cd3f0d35a38783
Nick Cross <ncross@redhat.com>
2022-06-28 19:39:35 +0100

    NCLSUP-682 Add Ivy to classpath run running Grab in groovy scripts

--- cli/src/test/resources/sample.groovy
+++ cli/src/test/resources/sample.groovy
@@ -3,8 +3,16 @@ import org.commonjava.maven.ext.core.groovy.InvocationPoint
 import org.commonjava.maven.ext.core.groovy.InvocationStage
 import org.jboss.gm.common.groovy.BaseScript
 
+@GrabResolver(name='private', root='https://maven.repository.redhat.com/techpreview/all/')
+@Grab('org.zeroturnaround:zt-exec:1.10')
+
 @InvocationPoint(invocationPoint = InvocationStage.FIRST)
 @GMEBaseScript BaseScript gmeScript
 
 println("Running Groovy script on " + gmeScript.getBaseDir())
 println("Known properties " + gmeScript.getUserProperties())
+
+// Hack to avoid using imports and groovy script causing errors in IntelliJ.
+Class c = Class.forName("org.zeroturnaround.exec.ProcessExecutor")
+def pExecutor = c.newInstance()
+pExecutor.exitValueAny()

72981873fc6d7b052fda28c4746fc759b609a9fb
Nick Cross <ncross@redhat.com>
2022-07-22 12:28:22 +0100

    Switch to lazy configuration

--- analyzer/build.gradle.kts
+++ analyzer/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     testImplementation("org.eclipse.jgit:org.eclipse.jgit:6.0.0.202111291000-r")
 }
 
-tasks.withType<Test> {
+tasks.withType<Test>().configureEach {
     systemProperties["jdk.attach.allowAttachSelf"] = "true"
 }
 

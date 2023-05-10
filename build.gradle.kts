b62cb29e7ce5ab82f42dc23e188ed71570373aef
jmichalo <jmichalo@redhat.com>
2023-05-10 15:45:33 +0200

    bump PME to 4.17-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -211,7 +211,7 @@ subprojects {
     extra["mavenVersion"] = "3.5.0"
     extra["opentelemetryVersion"] = "1.2.0"
     extra["ownerVersion"] = "1.0.12"
-    extra["pmeVersion"] = "4.16-SNAPSHOT"
+    extra["pmeVersion"] = "4.17-SNAPSHOT"
     extra["slf4jVersion"] = "1.7.30"
     extra["systemRulesVersion"] = "1.19.0"
 

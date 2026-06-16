22db07cbd743215b137b5ce83668e6a055b7bbaa
Nick Cross <ncross@redhat.com>
2026-06-16 12:35:38 +0100

    Remove redundant dependencies. Fixes for PME. Fix kotlin issue with OTEL.

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -27,6 +27,8 @@ dependencies {
         exclude(group = "com.fasterxml.jackson.core", module = "jackson-databind")
         exclude(group = "ch.qos.logback")
         exclude(group = "org.commonjava.maven.galley")
+        // Exclude until new release due to Quarkus bom
+        exclude(group = "org.jboss.pnc.otel")
     }
 
     implementation("org.jboss.pnc.maven-manipulator:pom-manipulation-core:${project.extra.get("pmeVersion")}") {
@@ -40,13 +42,24 @@ dependencies {
         exclude(group = "com.fasterxml.jackson.core", module = "jackson-databind")
         exclude(group = "ch.qos.logback")
         exclude(group = "org.commonjava.maven.galley")
+        // Exclude until new release due to Quarkus bom
+        exclude(group = "org.jboss.pnc.otel")
     }
 
     runtimeOnly("org.apache.maven:maven-core:${project.extra.get("mavenVersion")}")
     runtimeOnly("org.apache.maven:maven-model:${project.extra.get("mavenVersion")}")
     runtimeOnly("org.apache.maven:maven-artifact:${project.extra.get("mavenVersion")}")
 
-    implementation("org.jboss.pnc.otel:opentelemetry-ext-cli-java:${project.extra.get("opentelemetryVersion")}")
+    // This is a gigantic hack to avoid "Protocol message contained an invalid tag (zero).". The otel dependency
+    // contains kotlin-stdlib:2.x which conflicts horribly with the TestKit.
+    // Note for future - tried various combinations including an explicit kotlin override and
+    //  an exclusion for kotlin but none worked.
+    if (GradleVersion.current() == GradleVersion.version("6.5.1")) {
+        logger.warn("Using older opentelemetry-ext-cli-java for 6.5.1")
+        implementation("org.jboss.pnc.otel:opentelemetry-ext-cli-java:2.0.0")
+    } else {
+        implementation("org.jboss.pnc.otel:opentelemetry-ext-cli-java:${project.extra.get("opentelemetryVersion")}")
+    }
 
     // This is to prevent compilation errors in conjunction with Lombok due to use of PME code.
     compileOnly("org.apache.maven:maven-compat:${project.extra.get("mavenVersion")}")
@@ -55,5 +68,4 @@ dependencies {
     testFixturesCompile("org.assertj:assertj-core:${project.extra.get("assertjVersion")}")
     testImplementation("junit:junit:${project.extra.get("junitVersion")}")
     testImplementation("uk.org.webcompere:system-stubs-junit4:${project.extra.get("systemStubsVersion")}")
-    testImplementation(gradleApi())
 }
